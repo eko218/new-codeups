aa7e43d592a577b5509cce653c4a267615c52730
eko <e_c.n_v@icloud.com>
2024-01-17 22:03:18 +0900

    全体の調整まで

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,30 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+      // ヘッダーの高さ分だけコンテンツを下げる
 
+  $(function () {
+    // ヘッダーの高さ取得
+    const headerHeight = $('.js-header').height();
+    $('a[href^="#"]').click(function () {
+      const speed = 600;
+      let href = $(this).attr('href');
+      let target = $(href === '#' || href === '' ? 'html' : href);
+
+      // スクロール位置の計算
+      let position;
+      if (href === '.mv') {
+        // mvセクションへのリンクの場合、ヘッダーの高さは考慮しない
+        position = target.offset().top;
+      } else {
+        // それ以外の場合、ヘッダーの高さを差し引く
+        position = target.offset().top - headerHeight;
+      }
+
+      $('body,html').animate({ scrollTop: position }, speed, 'swing');
+      return false;
+    });
+  });
+});
 
       $(function () {
         // ハンバーガーメニュー
@@ -18,7 +42,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         }
         });
       });
-    });
 
     // mvスライダー
      var swiper = new Swiper(".js-mv-swiper", {
@@ -30,8 +53,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
             },
             speed: 2000, // ２秒かけながら次の画像へ移動
      });
-
-  
 
     //  キャンペーンスライダー
     jQuery(function ($) {
@@ -68,77 +89,15 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         breakpoints: {
           768: {
             spaceBetween: 40,
-          },
-          // 1920: {
-          //   slidesPerView: "5",
-          //   spaceBetween: 40,
-          // },
+          }
         },
-    
+        
         navigation: {
           nextEl: '.campaign__button-next',
           prevEl: '.campaign__button-prev',
           clickable: true,
         },
       });
-
-      // const campaignSwiper = new Swiper(".js-campaign-swiper", {
-      //   loop: true,
-      //   spaceBetween: 24,
-      //   slidesPerView: "1",
-      //   speed: 3000,
-      //   loopAdditionalSlides: 2,
-      //   width: 280,
-    
-      //   autoplay: {
-      //     delay: 0,
-      //     disableOnInteraction: false,
-      //   },
-      //   breakpoints: { //ブレークポイントの設定 小さい順に設定する！！
-      //     768: {
-      //       slidesPerView: "3.5",
-      //       spaceBetween: 40,
-      //       width: 1265.5,
-    
-      //     },
-      //     1920: {
-      //       slidesPerView: "5",
-      //       spaceBetween: 40,
-      //       width: 1825,
-      //     },
-      //   },
-      //   // 前後の矢印
-      //   navigation: {
-      //     nextEl: ".campaign__button-next",
-      //     prevEl: ".campaign__button-prev",
-      //   },
-      // });
-
-// //要素の取得とスピードの設定
-// var box = $('.js-colorbox'),
-//     speed = 700;  
- 
-// //.colorboxの付いた全ての要素に対して下記の処理を行う
-// box.each(function(){
-//     $(this).append('<div class="color"></div>')
-//     var color = $(this).find($('.color')),
-//     image = $(this).find('img');
-//     var counter = 0;
- 
-//     image.css('opacity','0');
-//     color.css('width','0%');
-//     //inviewを使って背景色が画面に現れたら処理をする
-//     color.on('inview', function(){
-//         if(counter == 0){
-// 　　　　　$(this).delay(200).animate({'width':'100%'},speed,function(){
-//                    image.css('opacity','1');
-//                    $(this).css({'left':'0' , 'right':'auto'});
-//                    $(this).animate({'width':'0%'},speed);
-//                 })
-//             counter = 1;
-//           }
-//      });
-// });
 
 $(function () {
   let box = $('.js-colorbox');
@@ -169,51 +128,6 @@ box.each(function () {
 });
 });
 
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   let boxes = document.querySelectorAll('.js-colorbox');
-//   let speed = 700;
-
-//   boxes.forEach(function (box) {
-//       let colorDiv = document.createElement('div');
-//       colorDiv.classList.add('color');
-//       box.appendChild(colorDiv);
-
-//       let color = box.querySelector('.color');
-//       let image = box.querySelector('img');
-//       let executed = false;
-
-//       image.style.opacity = '0';
-//       color.style.width = '0%';
-
-//       // 代替の "inview" イベントの実装
-//       window.addEventListener('scroll', function () {
-//           if (!executed && isElementInView(color)) {
-//               setTimeout(function () {
-//                   color.style.width = '100%';
-//                   setTimeout(function () {
-//                       image.style.opacity = '1';
-//                       color.style.left = '0';
-//                       color.style.right = 'auto';
-//                       color.style.width = '0%';
-//                   }, speed);
-//               }, 200);
-//               executed = true;
-//           }
-//       });
-//   });
-
-//   function isElementInView(element) {
-//       const rect = element.getBoundingClientRect();
-//       return (
-//           rect.top >= 0 &&
-//           rect.left >= 0 &&
-//           rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//           rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//       );
-//   }
-// });
-
 // Topへ戻るボタン
 $(function () {
   const pageTop = $('.js-page-top');
@@ -237,3 +151,5 @@ $(function () {
   });
 });
 });
+
+
